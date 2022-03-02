fc66d0bb6df5868bc4afac2603bb3d64b45f5f58
Marcos Couto <marcosmartinellicouto@gmail.com>
2022-03-02 10:57:05 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Web Client Example
+
+## Tutorial
+
+> https://www.baeldung.com/spring-5-webclient
+
+## Required
+
+- Java 14 +
+- Maven 3.8.x
---
+++ src/main/java/com/marcoscouto/webclient/WebclientApplication.java
@@ -0,0 +1,13 @@
+package com.marcoscouto.webclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebclientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebclientApplication.class, args);
+	}
+
+}
